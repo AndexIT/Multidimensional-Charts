fd6b86ac2d01053650f93d73f3b1f9faaf592aed
AndreaQuagliana <andreaquagliana111@gmail.com>
2024-04-21 12:16:07 +0200

    minor changes project name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Multi-Dimensional-Charts
+# Multidimensional-Charts
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.0.3.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Multi-Dimensional-Charts'),
+      dir: require('path').join(__dirname, './coverage/Multidimensional-Charts'),
       subdir: '.',
       reporters: [
         { type: 'html' },
